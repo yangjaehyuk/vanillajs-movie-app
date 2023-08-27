b1a11f26a3f46e8b253e5d4ba6b42b9872a6482f
yangjaehyuk <gndksakxk10@naver.com>
2023-08-28 01:41:48 +0900

    Fix: Fix some errors

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,4 @@
-import { Store } from "../core/heropy";
+import { Store } from '../core/heropy'
 
 const store = new Store({
   searchText: '',
@@ -14,51 +14,47 @@ export default store
 export const searchMovies = async page => {
   store.state.loading = true
   store.state.page = page
-  if(page===1){
+  if (page === 1) {
     store.state.movies = []
     store.state.message = ''
   }
-  try{
-    const res = await fetch('/api/movie',{
-      method: 'GET',
-      body:JSON.stringify({
+  try {
+    // const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
         title: store.state.searchText,
         page
       })
     })
-    const {Search, totalResults, Response, Error} = await res.json()
-    if(Response === 'True'){
+    const { Response, Search, totalResults, Error } = await res.json()
+    if (Response === 'True') {
       store.state.movies = [
         ...store.state.movies,
         ...Search
       ]
-      store.state.pageMax = Math.ceil(Number(totalResults)/10)
-    }
-    else{
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
       store.state.message = Error
+      store.state.pageMax = 1
     }
-  }catch(error){
-    console.log(error)
-  }finally{
+  } catch (error) {
+    console.log('searchMovies error:', error)
+  } finally {
     store.state.loading = false
   }
 }
-
 export const getMovieDetails = async id => {
-  try{
-    
-    const res = await fetch('/api/movie',{
+  try {
+    // const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
       method: 'POST',
-      body: JSON.stringify({})
+      body: JSON.stringify({
+        id
+      })
     })
-    store.state.movie = await res.json({
-      id
-    })
-  }
-  catch{
-    console.log(error)
-  }
-  finally{
-
+    store.state.movie = await res.json()
+  } catch (error) {
+    console.log('getMovieDetails error:', error)
   }
 }
